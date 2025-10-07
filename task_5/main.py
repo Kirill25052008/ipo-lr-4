20d093bbcf5bc29db43b0ebe230b358f2a1cdadd
Кирилл <129333736+Kirill25052008@users.noreply.github.com>
2025-10-08 00:22:53 +0300

    Update main.py

---
+++ task_5/main.py
@@ -0,0 +1,11 @@
+#Темник Кирилл
+import math
+
+list = [-12, 79, 4, 6, 854, 76, -4, 10, 43, 2, -35, 20, 5, 1, 94]
+count = 0
+
+for i in list:
+    if i > 10:
+        count = count + 1
+
+print(count)
