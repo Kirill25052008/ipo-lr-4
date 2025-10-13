a87415ead786e565750f897374cca7edf49ae609
Кирилл <129333736+Kirill25052008@users.noreply.github.com>
2025-10-13 14:34:40 +0300

    Update main.py

--- task_3/main.py
+++ task_3/main.py
@@ -1,5 +1,6 @@
 #Кирилл
-import math
+#Вариант 3
+import math# Добавляем библиотеку
 
 number = int(input("Введите число "))
 i = 0
@@ -8,4 +9,5 @@ j = 1
 while i < number:
     print (i)
     i, j = j, i + j
+
 
