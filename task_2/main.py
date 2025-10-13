93fbffbd6b0422a9f197c66b207522ebb9448feb
Кирилл <129333736+Kirill25052008@users.noreply.github.com>
2025-10-13 14:33:00 +0300

    Update main.py

--- task_2/main.py
+++ task_2/main.py
@@ -1,9 +1,10 @@
 #Кирилл
-import math
+import math# Добавляем библиотеку 
 
-list1 = [i for i in range(1, 11)]
-print(list1)
+list1 = [i for i in range(1, 11)]# создаём список1
+print(list1)#Выводим список1
 
-list2 = [pow(a,2) for a in list1]
-print(list2)
+list2 = [pow(a,2) for a in list1]#Создаём список2 из квадратов чисел из списка1
+print(list2)#Выводим список2
+
 
