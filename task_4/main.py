221e9f72d09ca31840e7c86974a41d1c2ecf250d
Кирилл <129333736+Kirill25052008@users.noreply.github.com>
2025-10-23 20:19:50 +0300

    Update main.py

--- task_4/main.py
+++ task_4/main.py
@@ -1,9 +1,10 @@
 #Кирилл
 #Вариант 3
-import math
+import math# Добавляем библиотеку
 
-float_list = [0.84, 4.142, 84.3, -6.084]
-int_list = [int(a) for a in float_list]
-print(int_list)
+float_list = [0.84, 4.142, 84.3, -6.084]# Создаём список вещественных чисел
+int_list = [int(a) for a in float_list]# Создаём генератор нового списка, который возвращает целую часть из первого списка
+print(int_list)# Выводим новый список
+
 
 
